2d61b26e53a267473c96fcfa3547b35ce5513d99
tkdgnszhekr2 <tkdgnszhekr@gmail.com>
2019-07-22 23:37:45 +0900

    Create Character fsm(idle,move)

---
+++ Revenant/Assets/Script/PlayerIdleState.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : IState
+{
+    private Player player;
+    void IState.OnEnter(Player player)
+    {
+        //player 프로퍼티 초기화
+        this.player = player;
+        // 초기화 구현
+    }
+    void IState.Update()
+    {
+        // 실행할것 구현
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            player.SetState(new PlayerMoveState());
+        }
+    }
+    void IState.OnExit()
+    {
+        //종료되면서 정리해야할것 구현
+    }
+}
+
